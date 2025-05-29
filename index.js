f64ceb7a655cf9ae90fe438cc0b9b380ed1cef41
noboKumar <nobokumar227@gmail.com>
2025-05-30 01:08:20 +0600

    add jobs and job details API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
 
@@ -8,10 +9,62 @@ const port = process.env.port || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send(`server is running... port: ${port}`)
-})
+// mongoDB
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`);
-})
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.bmuc12j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const jobsCollection = client.db("career_Code").collection("jobs");
+    const applicationsCollection = client
+      .db("career_Code")
+      .collection("applications");
+
+    // job API
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Applications API
+    app.post("/applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationsCollection.insertOne(application);
+      res.send(result);
+    });
+
+    app.get("/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant: email };
+      const result = await applicationsCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(`server is running... port: ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
